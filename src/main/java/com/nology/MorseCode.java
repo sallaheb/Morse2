34fc6cc87b94460ebd0617abf746c763a044d49b
Ebrima Sallah <sallahebrima@ymail.com>
2022-05-16 20:56:26 +0100

    git test

---
+++ src/main/java/com/nology/MorseCode.java
@@ -0,0 +1,59 @@
+package com.nology;
+
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+
+import java.util.HashMap;
+
+public class MorseCode {
+    public BiMap<String, String> CodeTranslator = HashBiMap.create();;
+
+    public MorseCode() {
+        this.CodeTranslator.put("A", ".- ");
+        this.CodeTranslator.put("B", "-... ");
+        this.CodeTranslator.put("C", "-.-. ");
+        this.CodeTranslator.put("D", "-.. ");
+        this.CodeTranslator.put("E", ". ");
+        this.CodeTranslator.put("F", "..-. ");
+        this.CodeTranslator.put("G", "--. ");
+        this.CodeTranslator.put("H", ".... ");
+        this.CodeTranslator.put("I", ".. ");
+        this.CodeTranslator.put("J", ".--- ");
+        this.CodeTranslator.put("K", "-.- ");
+        this.CodeTranslator.put("L", ".-.. ");
+        this.CodeTranslator.put("M", "-- ");
+        this.CodeTranslator.put("N", "-. ");
+        this.CodeTranslator.put("O", "--- ");
+        this.CodeTranslator.put("P", ".--. ");
+        this.CodeTranslator.put("Q", "--.- ");
+        this.CodeTranslator.put("R", ".-. ");
+        this.CodeTranslator.put("S", "... ");
+        this.CodeTranslator.put("T", "- ");
+        this.CodeTranslator.put("U", "..- ");
+        this.CodeTranslator.put("V", "...- ");
+        this.CodeTranslator.put("W", ".-- ");
+        this.CodeTranslator.put("X", "-..- ");
+        this.CodeTranslator.put("Y", "-.-- ");
+        this.CodeTranslator.put("Z", "--.. ");
+        this.CodeTranslator.put("0", "----- ");
+        this.CodeTranslator.put("1", ".---- ");
+        this.CodeTranslator.put("2", "..--- ");
+        this.CodeTranslator.put("3", "...-- ");
+        this.CodeTranslator.put("4", "....- ");
+        this.CodeTranslator.put("5", "..... ");
+        this.CodeTranslator.put("6", "-.... ");
+        this.CodeTranslator.put("7", "--... ");
+        this.CodeTranslator.put("8", "---.. ");
+        this.CodeTranslator.put("9", "----. ");
+        this.CodeTranslator.put(" ", " ");
+    }
+
+    public String convertToMorseCode(String string) {
+        return string;
+    }
+
+    public String convertToEnglish(String morseCode) {
+        return morseCode;
+    }
+
+}
---
+++ src/test/java/com/nology/MorseCodeTest.java
@@ -0,0 +1,55 @@
+package com.nology;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MorseCodeTest {
+
+    private MorseCode morseCode;
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        morseCode = new MorseCode();
+    }
+
+    @org.junit.jupiter.api.Test
+    void ShouldConvertToMorseCodeSingleCharacter() {
+        String result = morseCode.CodeTranslator.get("A");
+        assertEquals(result, ".- ");
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldConvertToMorseCodeMultipleCharacters() {
+
+        String result = "";
+        String word = "WAR";
+
+        for (int i = 0; i < word.length(); i++) {
+            Character character = Character.toUpperCase(word.charAt(i));
+            result += morseCode.CodeTranslator.get(character.toString());
+        }
+
+        assertEquals(result, ".-- .- .-. ");
+    }
+
+    @org.junit.jupiter.api.Test
+    void ShouldConvertToEnglishSingleCharacter() {
+
+        String result = morseCode.CodeTranslator.inverse().get(".-- ");
+
+        assertEquals(result, "W");
+    }
+
+    @org.junit.jupiter.api.Test
+    public void ShouldConvertToEnglishMultipleCharacters() {
+        String code = ".-- .- .-. ";
+        String[] characters = code.split(" ");
+        String result = "";
+        for (int i = 0; i < characters.length; i++) {
+            result += morseCode.CodeTranslator.inverse().get(characters[i] + " ");
+        }
+
+        assertEquals(result, "WAR");
+    }
+
+
+
+}
